e7a7b9036a3936ea2def631e0ffd286535088762
劉家均 <steven60344@gmail.com>
2025-03-07 22:49:29 +0800

    fix, add: button, dir, color

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,28 +14,6 @@ const PARAMS = {
 const minmax = { min: 0, max: 200, step: 1 };
 const minmax50 = { min: 0, max: 2, step: 0.01 };
 
-window.addEventListener("load", () => {
-    if (typeof Tweakpane === "undefined") {
-        console.error("Tweakpane is not loaded. Make sure it is included in HTML.");
-        return;
-    }
-
-    const pane = new Tweakpane.Pane();
-    const f1 = pane.addFolder({ title: "Controls" });
-
-    for (const [property, value] of Object.entries(PARAMS)) {
-        if (PARAMS.diffscale.includes(property)) {
-            f1.addInput(PARAMS, property, minmax50).on("change", (val) => {
-                setCustomProp(property, val.value);
-            });
-        } else {
-            f1.addInput(PARAMS, property, minmax).on("change", (val) => {
-                setCustomProp(property, val.value);
-            });
-        }
-    }
-});
-
 function setCustomProp(prop, val) {
     // 確保 opacity 和 speed 變數不加 px
     if (["opacity", "speedx", "speedy"].includes(prop)) {
