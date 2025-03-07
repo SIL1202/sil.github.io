929831108fe2d04ffe21f4764da9cd8620903f1d
劉家均 <steven60344@gmail.com>
2025-03-07 19:39:54 +0800

    fix: page cannot deploy

--- main.js
+++ main.js
@@ -1,56 +1,46 @@
 console.clear();
 
 const PARAMS = {
-  movex: 100,
-  movey: 100,
-  speedx: 0.16,
-  speedy: 0.48,
-  opacity: 0.3,
-  bgcolor: "#aaaaaa",
-  bgcolor2: "#444444",
-  diffscale: [
-    "speedx",
-    "speedy",
-    "opacity",
-    "trackroundness",
-    "thumbradius"
-  ]
+    movex: 100,
+    movey: 100,
+    speedx: 0.16,
+    speedy: 0.48,
+    opacity: 0.3,
+    bgcolor: "#aaaaaa",
+    bgcolor2: "#444444",
+    diffscale: ["speedx", "speedy", "opacity"]
 };
 
 const minmax = { min: 0, max: 200, step: 1 };
 const minmax50 = { min: 0, max: 2, step: 0.01 };
 
-const pane = new Tweakpane.Pane();
+window.addEventListener("load", () => {
+    if (typeof Tweakpane === "undefined") {
+        console.error("Tweakpane is not loaded. Make sure it is included in HTML.");
+        return;
+    }
 
-const f1 = pane.addFolder({
-  title: 'Controls',
+    const pane = new Tweakpane.Pane();
+    const f1 = pane.addFolder({ title: "Controls" });
+
+    for (const [property, value] of Object.entries(PARAMS)) {
+        if (PARAMS.diffscale.includes(property)) {
+            f1.addInput(PARAMS, property, minmax50).on("change", (val) => {
+                setCustomProp(property, val.value);
+            });
+        } else {
+            f1.addInput(PARAMS, property, minmax).on("change", (val) => {
+                setCustomProp(property, val.value);
+            });
+        }
+    }
 });
-// loop through all PARAMS and add onChange handler
-for (const [property, value] of Object.entries(PARAMS)) {
-  // console.log("xxx", `${property}::: ${PARAMS[property]}`);
-  if (isInArray(property, PARAMS.diffscale)) {
-    // console.log("diffscale");
-    f1.addInput(PARAMS, `${property}`, minmax50).on("change", (val) => {
-      setCustomProp(`${property}`, val.value);
-    });
-  } else {
-    // run the property through the onChange and hook up with custom function
-    f1.addInput(PARAMS, `${property}`, minmax).on("change", (val) => {
-      setCustomProp(`${property}`, val.value);
-    });
-  }
+
+function setCustomProp(prop, val) {
+    // 確保 opacity 和 speed 變數不加 px
+    if (["opacity", "speedx", "speedy"].includes(prop)) {
+        document.documentElement.style.setProperty("--" + prop, val);
+    } else {
+        document.documentElement.style.setProperty("--" + prop, val + "px");
+    }
 }
-
-function setCustomProp(prop, val, unit = "") {
-  var elem = document.querySelector('input[type="range"]');
-  // if(typeof val == 'number') { unit = 'px' }
-  document.documentElement.style.setProperty("--" + prop, val + unit);
-}
-
-//FUNCTIONS
-function isInArray(value, array) {
-  return array.indexOf(value) > -1;
-}
-
-// Fullscreen - idea from here: https://codepen.io/nocni_sovac/pen/bGMmJqR/57c415999e9f8fcd0d3eb635433fc63e?editors=1010
-
